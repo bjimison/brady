0f191f6121901c3ad60aa24a38df08dcf94b5e15
bjimison <brady.t.jimison@gmail.com>
2019-04-10 18:09:52 -0700

    imported Picture Container, Switch, Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import PictureContainer from "./containers/HomeContainer";
 
 class App extends Component {
   render() {
