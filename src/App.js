a7ae289c08d81e9a3c9a7390ee9d7871f6aac37e
bjimison <brady.t.jimison@gmail.com>
2019-04-24 20:43:47 -0700

    added photos to landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Big_Sur from "./images/camping:bigsur:cimbing/people_BigSur_Clouds.JPG";
+import above_the_clouds from "./images/camping:bigsur:cimbing/above_the_clouds.JPG";
+import less_mtn from "./images/camping:bigsur:cimbing/less_mtn.JPG";
+import naked_trees from "./images/camping:bigsur:cimbing/naked_trees.JPG";
 import logo from "./logo.svg";
 import "./App.css";
 import PictureContainer from "./containers/PictureContainer";
@@ -10,6 +13,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img id="home-image" src={Big_Sur} alt="logo" />
+          <img id="home-image" src={above_the_clouds} alt="logo" />
+          <img id="home-image" src={less_mtn} alt="logo" />
+          <img id="home-image" src={naked_trees} alt="logo" />
         </header>
       </div>
     );
