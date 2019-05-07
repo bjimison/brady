d56679be8f3aa463e9ae325fb81bfd2acf3236f6
bjimison <brady.t.jimison@gmail.com>
2019-05-06 17:36:04 -0700

    adjusted Switch statement back to App.js so it can pass down Link to be used in HomeContainer Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomeContainer from "./containers/HomeContainer";
+import PictureContainer from "./containers/PictureContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HomeContainer />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/pictures" component={PictureContainer} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import PictureContainer from "./PictureContainer";
 import less_mtn from "../images/camping:bigsur:cimbing/less_mtn.JPG";
@@ -11,11 +11,6 @@ class HomeContainer extends Component {
         <header className="App-header">
           <img className="home-image" src={less_mtn} alt="logo" />
         </header>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/pictures" component={PictureContainer} />
-          </Switch>
-        </BrowserRouter>
       </div>
     );
   }
